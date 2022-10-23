ea102a1ef57eb998fe2af56be4317c0b5d7268b0
Zhytnikov Egor <zhitikovegor06032002@gmail.com>
2022-10-23 16:02:59 +0300

    Lab2 v2

---
+++ CrossplatformLab2/CrossplatformLab2/Cell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossplatformLab2
+{
+    internal class Cell
+    {
+        public int Value { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Cell(int value, int x, int y)
+        {
+            Value = value;
+            X = x;
+            Y = y;
+        }
+    }
+}
--- CrossplatformLab2/CrossplatformLab2/Program.cs
+++ CrossplatformLab2/CrossplatformLab2/Program.cs
@@ -1,7 +1,13 @@
-﻿class Program
+﻿
+using CrossplatformLab2;
+using System.Numerics;
+using System.Text;
+
+class Program
 {
     const string OuputPath = @"..\..\..\output.txt";
-    static char[,] completemap;
+    static string[] tasklines;
+
     public static void Main(string[] args)
     {
         using (StreamWriter sw = new StreamWriter(OuputPath, false))
@@ -12,94 +18,170 @@
         string path = Console.ReadLine();
         try
         {
-            string[] lines = File.ReadAllLines(path);
-            InitializeArray(lines);
-            SuccessMessage("Success.");
+            tasklines = File.ReadAllLines(path);
         }
         catch
         {
-            FailMessage("Invalid file path or input data.");
+            FailMessage("Inccorect file path.");
+            return;
         }
+        CheckInput();       
     }
 
-    public static void InitializeArray(string[] lines) 
+    public static void CheckInput()
     {
-        if (lines == null)
-            FailMessage("file is empty");
-        if (int.TryParse(lines[0], out int N) && int.TryParse(lines[0], out int line1) && int.TryParse(lines[0], out int line2) && int.TryParse(lines[0], out int line3))
+        //Проверка на буквы
+        for (int i = 0; i < tasklines.Length; i++)
         {
-            int[,] map = new int[N, N];
-            completemap = new char[N, N];
-            completemap[0, 0] = '#';
-            completemap[N-1, N-1] = '#';
-            List<char> array = new List<char>();
-            for (int i = 1; i < lines.Length; i++)
+            try
             {
-                foreach (var item in lines[i])
-                {
-                    array.Add(item);
-                }
+                int a = int.Parse(tasklines[i]);
             }
-            int counter = 0;
-            for (int i = 0; i < N; i++)
+            catch
             {
-                for (int j = 0; j < N; j++)
-                {
-                    int.TryParse(array[counter].ToString(),out int res);
-                    map[i, j] = res;
-                    counter++;
-                }
+                FailMessage($"Input error in line {i} : {tasklines[i]}");
             }
-            Solve(map,N-1,N-1);
-            string result = string.Empty;
-            for (int i = 0; i < N; i++)
-            {
-                for (int j = 0; j < N; j++)
-                {
-                    if (completemap[i, j] == '\0')
-                        result+=".";
-                    else
-                        result+="#";
-                }
-                result += "\n";
-            }
-            SuccessMessage(result);
         }
+        if (int.Parse(tasklines[0]) != tasklines.Length - 1 || tasklines[1].Length!= int.Parse(tasklines[0]))
+        {
+            FailMessage("Inccorect N.");
+            return;
+        }
+        char [,] path = FindPath(Solve(InitializeField()));
+        DrawPath(path);
+    }
+
+    static void DrawPath(char[,]path) 
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < path.GetLength(0); i++)
+        {
+            for (int j = 0; j < path.GetLength(1); j++)
+            {
+                if (path[i, j] == '\0')
+                {
+                    Console.Write('.');
+                    sb.Append(".");
+                }
+                else 
+                {
+                    Console.Write(path[i, j]);
+                    sb.Append("#");
+                }
+                
+            }
+            Console.WriteLine();
+            sb.Append("\n");
+        }
+        File.WriteAllText(OuputPath,sb.ToString());
+    }
+
+    static int [,] InitializeField() 
+    {
+        int[,] field = new int[int.Parse(tasklines[0]), int.Parse(tasklines[0])];
+        for (int i = 1; i < tasklines.Length; i++)
+        {
+            string line = tasklines[i];
+            for (int j = 0; j < line.Length; j++)
+            {
+                field[i - 1, j] = int.Parse(line[j].ToString());
+            }
+        }
+        for (int i = 0; i < field.GetLength(0); i++)
+        {
+            for (int j = 0; j < field.GetLength(0); j++)
+            {
+                Console.Write(field[i, j].ToString());
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+        return field;
+    }
+
+    static int [,] Solve(int[,] field) 
+    {
+        int[,] result = new int [field.GetLength(0),field.GetLength(1)];
+        List<Cell> coords = new List<Cell>();
+        coords.Add(new Cell(field[result.GetLength(0)-1, result.GetLength(0) - 1], result.GetLength(0) - 1, result.GetLength(0) - 1));
+        result[field.GetLength(0) - 1, field.GetLength(0) - 1] = field[field.GetLength(0) - 1, field.GetLength(0) - 1];
+        while (coords.Count!=0) 
+        {
+            int count = coords.Count;
+            int index = 0;
+            for (int h=0;h<count;h++)
+            {
+                if (coords.Count == 0)
+                    break;
+                if (coords[index].X - 1 >= 0)
+                {
+                    if (result[coords[index].X - 1, coords[index].Y] > coords[index].Value + field[coords[index].X - 1, coords[index].Y] || result[coords[index].X - 1, coords[index].Y] == 0)
+                    {
+                        coords.Add(new Cell(field[coords[index].X - 1, coords[index].Y] + coords[index].Value, coords[index].X - 1, coords[index].Y));
+                        result[coords[index].X - 1, coords[index].Y] = field[coords[index].X - 1, coords[index].Y] + coords[index].Value;
+                    }
+                    
+                }
+                if (coords[index].Y - 1 >= 0) 
+                {
+                    coords.Add(new Cell(field[coords[index].X, coords[index].Y - 1] + coords[index].Value, coords[index].X, coords[index].Y - 1));
+                    result[coords[index].X, coords[index].Y - 1] = field[coords[index].X , coords[index].Y-1] + coords[index].Value;
+                }
+                
+                coords.Remove(coords[index]);
+                /*for (int f = 0; f < result.GetLength(0); f++)
+                {
+                    for (int j = 0; j < result.GetLength(1); j++)
+                    {
+                        Console.Write(result[f, j].ToString() + " \t");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();*/
+            }
+        }
+        return result;
+    }
+
+    static char [,] FindPath(int[,] field) 
+    {
+        int fieldsize = field.GetLength(0) - 1;
+        char[,] path = new char[fieldsize+1,fieldsize+1];
+        Cell Finish = new Cell(0,0,0);
+        Cell currentcell = new Cell(field[fieldsize, fieldsize], fieldsize, fieldsize);
+        while (currentcell.X != Finish.X || currentcell.Y != Finish.Y) 
+        {
+            path[currentcell.X, currentcell.Y] = '#';
+            Cell top=null;
+            Cell left=null;
+            if (currentcell.X - 1 >= 0) 
+            {
+                top = new Cell(field[currentcell.X - 1, currentcell.Y], currentcell.X - 1, currentcell.Y);
+            }
+            if (currentcell.Y - 1 >= 0)
+            {
+                left = new Cell(field[currentcell.X, currentcell.Y - 1], currentcell.X, currentcell.Y - 1);
+            }
+            currentcell = ChooseCell(top,left);
+            
+        }
+        path[0, 0] = '#';
+        return path;
+    }
+
+    static Cell ChooseCell(Cell top, Cell left) 
+    {
+        if (top == null)
+            return left;
+        else if (left == null)
+            return top;
         else 
         {
-            Console.WriteLine("Wrong data");
+            if (top.Value >= left.Value)
+                return left;
+            else
+                return top;
         }
-        
-    }
-
-    public static int Solve(int[,] arr,int n,int m) 
-    {
-        while (n >= 0 && m >= 0) 
-        {
-            if (n == 0 && m == 0)
-                return 0;
-            else if (n - 1 < 0)
-            {
-                completemap[n, m - 1] = '#';
-                return Solve(arr, n, m - 1);
-            }
-            else if (m - 1 < 0)
-            {
-                completemap[n - 1, m] = '#';
-                return Solve(arr, n - 1, m);
-            }
-            else if (arr[n - 1, m] < arr[n, m - 1])
-            {
-                completemap[n-1,m]= '#';
-                return Solve(arr, n - 1, m);
-            } 
-            else 
-            {
-                completemap[n, m-1] = '#';
-                return Solve(arr, n, m - 1);
-            }
-        }
-        return 0;            
     }
 
     public static void SuccessMessage(string text)
